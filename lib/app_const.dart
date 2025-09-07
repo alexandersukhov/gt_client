6a5db324997575d0a96f7cd044b0e2bb3ebbca3c
alexander.sukhovbim@gmail.com <=>
2025-09-07 16:27:18 +0300

    app const class added

--- lib/app_const.dart
+++
@@ -1,4 +0,0 @@
-class AppConst {
-  static String measurement_route = "http://109.172.7.220:85/get_temp_data/measurements";
-  static String token_route = "http://109.172.7.220:85/get_temp_auth/token";
-}
